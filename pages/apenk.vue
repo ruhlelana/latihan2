e6d610a90bcaa9f6712f6c586e7789282dd5c6a2
ruhlelana <ruhlelana@gmail.com>
2022-12-07 00:40:31 +0700

    testing

---
+++ pages/apenk.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+<div class="columns">
+  <div class="column is-12"> 
+    <Slider/>
+  </div>
+  </div>
+  <div class="columns">
+  <div class="column is-12">
+    Second column
+  </div>
+  </div>
+  <div class="columns">
+  <div class="column is-12">
+    Third column
+  </div>
+  </div>
+  <div class="columns">
+  <div class="column is-12">
+    Fourth column
+  </div>
+  </div>
+</div>
+</div>
+</template>

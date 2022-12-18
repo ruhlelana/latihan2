2a3b3e3ef151896de623e233994935d1792b95ea
ruhlelana <ruhlelana@gmail.com>
2022-12-18 19:15:10 +0700

    vercel fix

--- components/Slider.vue
+++ components/Slider.vue
@@ -15,7 +15,7 @@ export default {
     data(){
         return {
             carousels: [
-                { text: 'Slide 1', color: 'primary' },
+                { text: 'Halo Dunia', color: 'primary' },
                 { text: 'Slide 2', color: 'info' },
                 { text: 'Slide 3', color: 'success' },
                 { text: 'Slide 4', color: 'warning' },
